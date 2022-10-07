e787f22debec6198a65f6e2d7a6e21b80ab9d2b9
tdarmawan <tdarmawan@hacktiv8.com>
2022-10-07 15:32:54 +0700

    test deploy golang

--- database/db.go
+++ database/db.go
@@ -5,16 +5,18 @@ import (
 	"gin-api/models"
 	"log"
 
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var (
-	host     = "containers-us-west-80.railway.app"
-	user     = "postgres"
-	password = "dIoZwlyuHiVVbT9vABVe"
-	dbPort   = "5583"
-	dbname   = "railway"
+	host = os.Getenv("DB_HOST")
+	user = os.Getenv("DB_USER")
+	password = os.Getenv("DB_PASSWORD")
+	dbPort = os.Getenv("DB_PORT")
+	dbname = os.Getenv("DB_NAME")
 	db       *gorm.DB
 	err      error
 )
